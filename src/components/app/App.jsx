0c8dd4f71c477c28179098eb6eb2cd241cf9e9f9
“ilroman” <il.roman@ukr.net>
2020-07-03 21:00:17 +0300

    font roboto

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -17,49 +17,56 @@ const App = () => {
                     <Popup
                         setPopup={setPopup}
                     />
-                </div>
-            }
+                </div>}
 
             <header className="header"></header>
             <main className="main">
                 <div className="passport">
-                    <div className="passport__image">
-
-                    </div>
-                    <div className="passport__code">
-
-                    </div>
+                    <div className="passport__image"></div>
+                    <div className="passport__code"></div>
                 </div>
                 <div className="navigation-wrapper">
                     <div className="navigation">
                         <div
-                            className={location.pathname === '/travel-documents' ? "navigation__item active" : "navigation__item"}
-                        >
+                            className={
+                                location.pathname === '/travel-documents'
+                                    ? "navigation__item navigation__item_active"
+                                    : "navigation__item"}>
                             <Link
                                 to="/travel-documents"
-                                className="link"
-                            >
-                                Travel Documents
+                                className={
+                                    location.pathname === '/travel-documents'
+                                        ? "link link_active"
+                                        : "link"}
+                            >Travel Documents
                             </Link>
                         </div>
                         <div
-                            className={location.pathname === '/loyalty-programs' ? "navigation__item active" : "navigation__item"}
-                        >
+                            className={
+                                location.pathname === '/loyalty-programs'
+                                    ? "navigation__item navigation__item_active"
+                                    : "navigation__item"}>
                             <Link
                                 to={popupIsOpened ? '/travel-documents' : "/loyalty-programs"}
-                                className="link"
-                            >
-                                Loyalty Programs
+                                className={
+                                    location.pathname === '/loyalty-programs'
+                                        ? "link link_active"
+                                        : "link"}
+                            >Loyalty Programs
                             </Link>
                         </div>
                         <div
-                            className={location.pathname === '/travel-preferences' ? "navigation__item active" : "navigation__item"}
-                        >
+                            className={
+                                location.pathname === '/travel-preferences'
+                                    ? "navigation__item navigation__item_active"
+                                    : "navigation__item"}>
                             <Link
                                 to={popupIsOpened ? '/travel-documents' : "/travel-preferences"}
-                                className="link"
-                            >
-                                Travel Preferences
+                                className={
+                                    location.pathname === '/travel-preferences'
+                                        ? "link link_active"
+                                        : "link"}
+                            >Travel Preferences
                             </Link>
                         </div>
                     </div>
@@ -67,9 +74,7 @@ const App = () => {
                         <Redirect to="/travel-documents" />
                     </Route>
                     <Route path="/travel-documents">
-                        <TravelDocuments
-                            setPopup={setPopup}
-                        />
+                        <TravelDocuments setPopup={setPopup} />
                     </Route>
                     <Route path="/loyalty-programs">
                         <div className="loyalty-programs"></div>
